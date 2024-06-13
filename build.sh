a36a9b8ad191df26b8a4a86c8f8d5bc6d1e432a5
Sunshine40 <sunshine_40@qq.com>
2024-06-13 13:36:13 +0800

    Html UI 本地化。英文版搜索改为 and 逻辑 (#46)
    * 翻新 README.md
    
    * 添加中英文语言切换功能
    
    * Html UI 本地化。英文版搜索改为 and 逻辑。

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mdbook build "$@"
-MDBOOK_OUTPUT__HTML__THEME=../theme MDBOOK_OUTPUT__HTML__ADDITIONAL_CSS='["../theme/nomicon.css", "../theme/language-picker.css"]' mdbook build en -d ../book/en
+MDBOOK_OUTPUT__HTML__THEME=../theme MDBOOK_OUTPUT__HTML__ADDITIONAL_CSS='["../theme/nomicon.css", "../theme/language-picker.css"]' MDBOOK_OUTPUT__HTML__SEARCH__USE_BOOLEAN_AND=true mdbook build en -d ../book/en
